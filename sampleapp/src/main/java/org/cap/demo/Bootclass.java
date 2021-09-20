4bb8537a61894a26820ca395c72b778eed5798a3
GunishettiAnusha <agunishe@LIN65004035.corp.capgemini.com>
2021-09-20 10:52:16 +0530

    show method added

--- sampleapp/src/main/java/org/cap/demo/Bootclass.java
+++ sampleapp/src/main/java/org/cap/demo/Bootclass.java
@@ -4,7 +4,12 @@ public class Bootclass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from sts");
+		show();
 
+	}
+	public static void show()
+	{
+	System.out.println("show method here1");
 	}
 
 }
